ef7f13df3a7449ca650781d335cb29f1d17b9baa
Dodgin <nick@zaron.technology>
2017-12-29 01:37:13 -0500

    main bootstrap addition

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from classes import bootstrap
 from classes import wandtracker
 import time
 import cv2
 
+# Bootstrap from config files
+bs = Bootstrap.config()
+config = bs.run()
+
+# Instantiate wandtracker TODO: this needs to be based on config loads
 wt = wandtracker.WandTracker('http://172.16.0.26:8080/stream/video.mjpeg');
 
 # Main Loop
